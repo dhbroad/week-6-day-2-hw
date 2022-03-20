2f801792515adc69b19ac6f24b5f9c66733ec395
dhbroad <dhbroad@udel.edu>
2022-03-19 21:10:49 -0400

    working submission

---
+++ app/models.py
@@ -0,0 +1,104 @@
+from email.policy import default
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug.security import generate_password_hash
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+# create our Models based off of our ERD
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(250), nullable=False)
+    post = db.relationship('Post', backref='author', lazy=True)
+    cart_item = db.relationship('Cart', backref='cart_user', lazy=True)
+    pokedex_pokemon = db.relationship('Pokedex', backref='pokedex_user', lazy=True)
+    is_admin = db.Column(db.Boolean, default=False)
+
+    def __init__(self, username, email, password, is_admin=False):
+        self.username = username
+        self.email = email
+        self.password = generate_password_hash(password)
+        self.is_admin=is_admin
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False, unique=False)
+    image = db.Column(db.String(300))
+    caption = db.Column(db.String(300))
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, title, image, caption, user_id):
+        self.title = title
+        self.image = image
+        self.caption = caption
+        self.user_id = user_id
+
+    
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(200), nullable=False, unique=False)
+    image = db.Column(db.String(300))
+    description = db.Column(db.String(300))
+    price = db.Column(db.Float())
+    cart_item = db.relationship('Cart', backref='cart_product', lazy=True)
+
+    def __init__(self, product_name, image, description, price):
+        self.product_name = product_name
+        self.image = image
+        self.description = description
+        self.price = price
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "product_name": self.product_name,
+            "image": self.image,
+            "description": self.description,
+            "price": self.price
+        }
+
+class Cart(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+
+    def __init__(self, user_id, product_id):
+        self.user_id = user_id
+        self.product_id = product_id
+
+
+class Pokemon(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pokemon_name = db.Column(db.String(50))
+    img_url = db.Column(db.String(300))
+    ability1 = db.Column(db.String(300))
+    ability2 = db.Column(db.String(300))
+    pokedex_pokemon = db.relationship('Pokedex', backref='pokedex_pokemon', lazy=True)
+
+    def __init__(self, pokemon_name, img_url, ability1, ability2):
+        self.pokemon_name = pokemon_name
+        self.img_url = img_url
+        self.ability1 = ability1
+        self.ability2 = ability2
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "pokemon_name": self.pokemon_name,
+            "img_url": self.img_url,
+            "ability1": self.ability1,
+            "ability2": self.ability2
+                    }
+
+class Pokedex(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    pokemon_id = db.Column(db.Integer, db.ForeignKey('pokemon.id'), nullable=False)
+
+    def __init__(self, user_id, pokemon_id):
+        self.user_id = user_id
+        self.pokemon_id = pokemon_id
---
+++ app/pokemon/routes.py
@@ -0,0 +1,65 @@
+from flask import Blueprint, redirect, render_template, request, url_for
+from flask_login import current_user, login_required
+import requests as r
+
+pokemon = Blueprint('pokemon', __name__, template_folder="pokemon_templates")
+
+from app.models import db, Pokemon, Pokedex
+from .forms import CreatePokemonForm
+
+
+@pokemon.route('/pokemon', methods=["POST"])
+def myPokemon():
+    name = request.form.to_dict()['name']
+    data = r.get(f"https://pokeapi.co/api/v2/pokemon/{name}")
+    if data.status_code == 200:
+        my_data = data.json()
+        abilities = my_data['abilities']
+        my_abilities = []
+        count = 1
+        for item in abilities:
+            print(item['ability']['name'])
+            my_abilities.append((item['ability']['name']))
+            if count == 1:
+                print(f"{item['ability']['name']} should be ability1")
+                ability1 = item['ability']['name']
+                print(f"{ability1} has been assigned to ability1")
+            elif count == 2:
+                print(f"{item['ability']['name']} should be ability2")
+                ability2 = item['ability']['name']
+                print(f"{ability2} has been assigned to ability2")
+            count += 1
+        my_img = my_data['sprites']['front_default']
+        pokemon = Pokemon.query.filter_by(pokemon_name=name).first()
+        if pokemon is None:
+            pokemon = Pokemon(name, my_img, ability1, ability2)
+            db.session.add(pokemon)
+            db.session.commit()
+        return render_template('pokemon.html', abilities=my_abilities, img_url=my_img, name=name, pokemon=pokemon)
+    return redirect(url_for('home'))
+
+
+# POKEDEX FUNCTIONALITY
+@pokemon.route('/pokedex')
+@login_required
+def showPokedex():
+    pokedex = Pokedex.query.filter_by(user_id=current_user.id)
+    count = {}
+    for apokemon in pokedex:
+        count[apokemon.pokemon_id] = count.get(apokemon.pokemon_id, 0) + 1
+    
+    pokedex_pokemon = []
+    for pokemon_id in count:
+        pokemon_info = Pokemon.query.filter_by(id=pokemon_id).first().to_dict()
+        pokedex_pokemon.append(pokemon_info)
+
+    return render_template('pokedex.html', pokedex = pokedex_pokemon)
+
+
+@pokemon.route('/pokedex/add/<int:pokemon_id>') # for adding from a list of pokemon
+def addToPokedex(pokemon_id):
+    pokedex_pokemon = Pokedex(current_user.id, pokemon_id)
+    db.session.add(pokedex_pokemon)
+    db.session.commit()
+    print(f"Pokemon added to Pokedex successfully!")
+    return redirect(url_for('home'))
